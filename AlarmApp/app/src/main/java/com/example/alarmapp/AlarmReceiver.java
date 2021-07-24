c628b8c3f909af9f550bd0aff83695366bcc365c
Kong Youngin <kongyi123@nate.com>
2021-07-24 19:11:06 +0900

    add.. everything that not be arranged.

--- AlarmApp/app/src/main/java/com/example/alarmapp/AlarmReceiver.java
+++ AlarmApp/app/src/main/java/com/example/alarmapp/AlarmReceiver.java
@@ -1,132 +1,76 @@
 package com.example.alarmapp;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
-import android.view.WindowManager;
-import android.widget.Toast;
+import android.util.Log;
 
-import java.lang.reflect.Array;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 
 import androidx.core.app.NotificationCompat;
 
-import static android.content.Context.MODE_PRIVATE;
-
 public class AlarmReceiver extends BroadcastReceiver {
+
+    private int NOTIFICATION_ID = 1;
+    private int REQUEST_CODE = 0;
+    private int FLAGS = 0;
 
     Context context;
     @Override
     public void onReceive(Context context, Intent intent) {
         // 알람을 BroadcastReceiver로 받는다.
         // 받으면 Noti를 주면됨.
-
         this.context = context;
-        // intent로부터 전달받은 string
-        String get_yout_string = intent.getExtras().getString("state");
-
-        // RingtonePlayingService 서비스 intent 생성
-        Intent service_intent = new Intent(context, RingtonePlayingService.class);
-
-        // RingtonePlayinService로 extra string값 보내기
-        service_intent.putExtra("state", get_yout_string);
-        // start the ringtone service
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O){
-            this.context.startForegroundService(service_intent);
-        }else{
-            this.context.startService(service_intent);
-        }
-
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Intent notificationIntent = new Intent(context, MainActivity.class);
-
-        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
-                | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        PendingIntent pendingI = PendingIntent.getActivity(context, 0,
-                notificationIntent, 0);
-
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
-
-
-        //OREO API 26 이상에서는 채널 필요
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-
-            builder.setSmallIcon(R.drawable.ic_launcher_foreground); //mipmap 사용시 Oreo 이상에서 시스템 UI 에러남
-
-
-            String channelName ="매일 알람 채널";
-            String description = "매일 정해진 시간에 알람합니다.";
-            int importance = NotificationManager.IMPORTANCE_HIGH; //소리와 알림메시지를 같이 보여줌
-
-            NotificationChannel channel = new NotificationChannel("default", channelName, importance);
-            channel.setDescription(description);
-
-            if (notificationManager != null) {
-                // 노티피케이션 채널을 시스템에 등록
-                notificationManager.createNotificationChannel(channel);
-            }
-        }else builder.setSmallIcon(R.mipmap.ic_launcher); // Oreo 이하에서 mipmap 사용하지 않으면 Couldn't create icon: StatusBarIcon 에러남
-
-
-        builder.setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setWhen(System.currentTimeMillis())
-
-                .setTicker("{Time to watch some cool stuff!}")
-                .setContentTitle("알림")
-                .setContentText("공영인이 만든 알림")
-                .setContentInfo("INFO")
-                .setContentIntent(pendingI)
-                .setOngoing(true)
-                .setPriority(NotificationCompat.PRIORITY_MAX);
-
-        if (notificationManager != null) {
-
-
-
-            // 노티피케이션 동작시킴
-            notificationManager.notify(1234, builder.build());
-
-            Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-
-            long[] timings = new long[]{1000L, 1000L, 1000L, 1000L, 1000L};
-            int[] amplitudes = new int[]{0, 100, 200, 100, 200};
-
-            //vibrator.vibrate(VibrationEffect.createOneShot(1000, 50));
-
-
-            vibrator.vibrate(VibrationEffect.createWaveform(timings, amplitudes,-1));
-
-
-            Calendar nextNotifyTime = Calendar.getInstance();
-
-            // 내일 같은 시간으로 알람시간 결정
-            nextNotifyTime.add(Calendar.DATE, 1);
-
-            //  Preference에 설정한 값 저장
-            SharedPreferences.Editor editor = context.getSharedPreferences("daily alarm", MODE_PRIVATE).edit();
-            editor.putLong("nextNotifyTime", nextNotifyTime.getTimeInMillis());
-            editor.apply();
-
-            Date currentDateTime = nextNotifyTime.getTime();
-            String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(currentDateTime);
-            Toast.makeText(context.getApplicationContext(),"다음 알람은 " + date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
-        }
+        Log.d("kyi123", "Alarm Received");
+        doNotify(); // 노티
+//        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.alim); // 소리
+//        mediaPlayer.start();
+        ringVibration(); // 진동
     }
+//
+    private void doNotify() {
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity(
+                        context,
+                        NOTIFICATION_ID,
+                        contentIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(
+                context,
+                context.getString(R.string.notification_channel_id)
+        );
+
+        builder.setSmallIcon(R.drawable.ic_launcher_background);
+        builder.setContentTitle(context.getString(R.string.app_name));
+        builder.setContentText("동일 간격 노티 알람 만들기!!");
+        builder.setContentIntent(contentPendingIntent);
+        builder.setAutoCancel(true);
+//        builder.setOngoing(true); // Ongoing 붙이면 wear로 noti 안감.
+
+//        builder.setStyle(R.drawable.ic_launcher_background);
+//        builder.setLargeIcon(R.drawable.ic_launcher_background);
+//        builder.addAction(R.drawable.ic_launcher_foreground,
+//                context.getString(R.string.app_name),
+//                );
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        NotificationManager notificationManager = context.getSystemService(
+                NotificationManager.class
+        );
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    private void ringVibration() {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        long[] timings = new long[]{1000L, 1000L, 1000L, 1000L, 1000L};
+        int[] amplitudes = new int[]{0, 100, 200, 100, 200};
+        //vibrator.vibrate(VibrationEffect.createOneShot(1000, 50));
+        vibrator.vibrate(VibrationEffect.createWaveform(timings, amplitudes,-1));
+    }
+
 }
--- AlarmApp/app/src/main/java/com/example/alarmapp/MainActivity.java
+++ AlarmApp/app/src/main/java/com/example/alarmapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.alarmapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
 
 import android.app.AlarmManager;
 import android.app.NotificationChannel;
@@ -10,165 +9,51 @@ import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
+import android.os.SystemClock;
+import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.TimePicker;
-import android.widget.Toast;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Locale;
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createChannel(
+                getString(R.string.notification_channel_id),
+                getString(R.string.notification_channel_name)
+        );
 
-//        PowerManager powerManager = (PowerManager)getSystemService(Context.POWER_SERVICE);
-//        PowerManager.WakeLock wakeLock = powerManager.newWakeLock( PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE, "WAKELOCK");
-//        wakeLock.acquire();
-
-        String channelName ="매일 알람 채널";
-        String description = "매일 정해진 시간에 알람합니다.";
-        int importance = NotificationManager.IMPORTANCE_HIGH; //소리와 알림메시지를 같이 보여줌
-
-        NotificationChannel channel = new NotificationChannel("default", channelName, importance);
-        channel.setDescription(description);
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if (notificationManager != null) {
-            // 노티피케이션 채널을 시스템에 등록
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
-        Intent notificationIntent = new Intent(this, MainActivity.class);
-        PendingIntent pendingI = PendingIntent.getActivity(this, 0,
-                notificationIntent, 0);
-
-
-        builder.setAutoCancel(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setTicker("{Time to watch some cool stuff!}")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("알림")
-                .setContentText("공영인이 만든 알림")
-                .setContentInfo("INFO")
-                .setContentIntent(pendingI)
-                .setOngoing(true)
-                .setPriority(NotificationCompat.PRIORITY_MAX);
-        notificationManager.notify(1234, builder.build());
-
-
-        final TimePicker picker=(TimePicker)findViewById(R.id.timePicker);
-        picker.setIs24HourView(true);
-
-
-        // 앞서 설정한 값으로 보여주기
-        // 없으면 디폴트 값은 현재시간
-        SharedPreferences sharedPreferences = getSharedPreferences("daily alarm", MODE_PRIVATE);
-        long millis = sharedPreferences.getLong("nextNotifyTime", Calendar.getInstance().getTimeInMillis());
-
-        Calendar nextNotifyTime = new GregorianCalendar();
-        nextNotifyTime.setTimeInMillis(millis);
-
-        Date nextDate = nextNotifyTime.getTime();
-        String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(nextDate);
-        Toast.makeText(getApplicationContext(),"[처음 실행시] 다음 알람은 " + date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
-
-
-        // 이전 설정값으로 TimePicker 초기화
-        Date currentTime = nextNotifyTime.getTime();
-        SimpleDateFormat HourFormat = new SimpleDateFormat("kk", Locale.getDefault());
-        SimpleDateFormat MinuteFormat = new SimpleDateFormat("mm", Locale.getDefault());
-
-        int pre_hour = Integer.parseInt(HourFormat.format(currentTime));
-        int pre_minute = Integer.parseInt(MinuteFormat.format(currentTime));
-
-
-        if (Build.VERSION.SDK_INT >= 23 ){
-            picker.setHour(pre_hour);
-            picker.setMinute(pre_minute);
-        }
-        else{
-            picker.setCurrentHour(pre_hour);
-            picker.setCurrentMinute(pre_minute);
-        }
-
-
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-
-                int hour, hour_24, minute;
-                String am_pm;
-                if (Build.VERSION.SDK_INT >= 23 ){
-                    hour_24 = picker.getHour();
-                    minute = picker.getMinute();
-                }
-                else{
-                    hour_24 = picker.getCurrentHour();
-                    minute = picker.getCurrentMinute();
-                }
-                if(hour_24 > 12) {
-                    am_pm = "PM";
-                    hour = hour_24 - 12;
-                }
-                else
-                {
-                    hour = hour_24;
-                    am_pm="AM";
-                }
-
-                // 현재 지정된 시간으로 알람 시간 설정
-                Calendar calendar = Calendar.getInstance();
-                calendar.setTimeInMillis(System.currentTimeMillis());
-                calendar.set(Calendar.HOUR_OF_DAY, hour_24);
-                calendar.set(Calendar.MINUTE, minute);
-                calendar.set(Calendar.SECOND, 0);
-
-                // 이미 지난 시간을 지정했다면 다음날 같은 시간으로 설정
-                if (calendar.before(Calendar.getInstance())) {
-                    calendar.add(Calendar.DATE, 1);
-                }
-
-                Date currentDateTime = calendar.getTime();
-                String date_text = new SimpleDateFormat("yyyy년 MM월 dd일 EE요일 a hh시 mm분 ", Locale.getDefault()).format(currentDateTime);
-                Toast.makeText(getApplicationContext(),date_text + "으로 알람이 설정되었습니다!", Toast.LENGTH_SHORT).show();
-
-                //  Preference에 설정한 값 저장
-                SharedPreferences.Editor editor = getSharedPreferences("daily alarm", MODE_PRIVATE).edit();
-                editor.putLong("nextNotifyTime", (long)calendar.getTimeInMillis());
-                editor.apply();
-
-
-                diaryNotification(calendar);
-            }
-
-        });
+        Log.d("kyi123", "here!");
     }
 
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), MyService.class);
+        intent.putExtra("command", "show");
+//        startService(intent);
+        startForegroundService(intent); // foreground service 실행을 위해 이것만 있으면 됨. 윗줄의 startService(intent)는 필요 없음.
+        diaryNotification();
+    }
 
-    void diaryNotification(Calendar calendar)
-    {
-//        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
-//        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-//        Boolean dailyNotify = sharedPref.getBoolean(SettingsActivity.KEY_PREF_DAILY_NOTIFICATION, true);
+    public void onClick2(View view) {
+        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+        alarmIntent.putExtra("state","alarm on");
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
+        // 알람 Broadcast Intent를 만든다. -> alarmManager를 통해 특정시각에 broadcast 날리도록 예약할것이다.
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        // 사용자가 매일 알람을 허용했다면
+        if (alarmManager != null) {
+            //alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent); // 예약
+            alarmManager.cancel(pendingIntent);
+        }
+    }
+
+    public void diaryNotification() {
         Boolean dailyNotify = true; // 무조건 알람을 사용
 
         PackageManager pm = this.getPackageManager();
@@ -178,44 +63,43 @@ public class MainActivity extends AppCompatActivity {
 
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, alarmIntent, 0);
         // 알람 Broadcast Intent를 만든다. -> alarmManager를 통해 특정시각에 broadcast 날리도록 예약할것이다.
-
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-
-
         // 사용자가 매일 알람을 허용했다면
+
+        EditText et = findViewById(R.id.edit_text_millisecond);
+        String text = et.getText().toString();
+        Long sec = Long.parseLong(text);
         if (dailyNotify) {
-
-
             if (alarmManager != null) {
-
-                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
-                        AlarmManager.INTERVAL_DAY, pendingIntent);
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent); // 예약
-                }
+                //alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent); // 예약
+                Log.d("kyi123", "sec = " + sec);
+                alarmManager.setInexactRepeating(
+                        AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                        SystemClock.elapsedRealtime(),
+                        sec,
+                        pendingIntent);
             }
-
             // 부팅 후 실행되는 리시버 사용가능하게 설정
             pm.setComponentEnabledSetting(receiver,
                     PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
                     PackageManager.DONT_KILL_APP);
-
         }
-//        else { //Disable Daily Notifications
-//            if (PendingIntent.getBroadcast(this, 0, alarmIntent, 0) != null && alarmManager != null) {
-//                alarmManager.cancel(pendingIntent);
-//                //Toast.makeText(this,"Notifications were disabled",Toast.LENGTH_SHORT).show();
-//            }
-//            pm.setComponentEnabledSetting(receiver,
-//                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
-//                    PackageManager.DONT_KILL_APP);
-//        }
     }
 
-    public void onClick(View view) {
-        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
-        alarmIntent.putExtra("state","alarm off");
+    void createChannel(String channelId, String channelName) {
+        Log.d("kyi123", "createChannel");
+        NotificationChannel notificationChannel = new NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_DEFAULT);
+        notificationChannel.setShowBadge(false);
+        notificationChannel.enableLights(true);
+        notificationChannel.setLightColor(Color.RED);
+        notificationChannel.enableVibration(true);
+        notificationChannel.setDescription(getString(R.string.breakfast_notification_channel_description));
+        NotificationManager notificationManager = getSystemService(
+                NotificationManager.class
+        );
+        notificationManager.createNotificationChannel(notificationChannel);
     }
-
 }
